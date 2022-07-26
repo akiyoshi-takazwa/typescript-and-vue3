595fd669d753424c010c4fe5bbbfa027b2e0db9b
akiyoshi-takazwa <takazawa@arsaga.jp>
2022-07-26 22:49:16 +0900

    create tweet logic

---
+++ src/components/TweetList.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import {defineEmits, defineProps} from "vue";
+
+type Tweet = {
+  id: number,
+  description: string
+}
+
+type Props = {
+  tweets: Tweet[],
+  deleteTweet: (id: number) => void,
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <li v-for="tweet in tweets" class="tweet-list" :key="tweet.id">
+    <span>{{ tweet.description }}</span>
+    <button @click="deleteTweet(tweet.id)" class="tweet-list_button">削除</button>
+  </li>
+</template>
+
+<style scoped>
+.tweet-list {
+  list-style: none;
+  display: flex;
+  background-color: thistle;
+  justify-content: space-between;
+  margin-bottom: 20px;
+  padding: 4px 0 4px 10px;
+  width: 500px;
+}
+
+</style>
---
+++ src/components/TweetPostForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="form-container">
+    <input v-model="inputDescription" />
+    <button class="form-container_button" @click="postTweet">POST</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {defineEmits, ref} from "vue";
+
+const inputDescription = ref<string>('');
+
+// 子供から親へデータを渡す
+const emit = defineEmits(['post-tweet'])
+const postTweet = () => {
+  emit('post-tweet', inputDescription.value, 'wwww')
+};
+
+</script>
+
+<style scoped>
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: aliceblue;
+  padding: 24px 0;
+  width: 60%;
+  margin-bottom: 12px;
+  border-radius: 4px;
+}
+
+
+.form-container_button{
+  width: 80px;
+  background-color: lightskyblue;
+  color: #fff;
+  padding: 2px 10px;
+  margin-top: 5px;
+}
+
+</style>
